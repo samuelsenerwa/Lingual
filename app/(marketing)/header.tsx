3268201a4ee4fcf04e78590f6d2e34496ba78222
samuelsenerwa <samuelsenerwa@gmail.com>
2024-05-27 23:31:11 +0300

    feat: Working on header

---
+++ app/(marketing)/header.tsx
@@ -0,0 +1,7 @@
+// for naming components I prefer to use export naming
+
+export const Header = () => {
+  return (
+    <header className="h-20 w-full border-b-2 border-slate-200 px-4"></header>
+  );
+};
--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,9 +1,5 @@
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
-  return (
-    <div>
-      <Button> CLick me</Button>
-    </div>
-  );
+  return <div>marketing</div>;
 }
