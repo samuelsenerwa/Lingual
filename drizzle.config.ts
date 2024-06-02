ff9f79fdb80f3aeec22842c12c2cab86e6861383
samuelsenerwa <samuelsenerwa@gmail.com>
2024-06-02 15:28:55 +0300

    feat: configured drizzle and created first migration

---
+++ drizzle.config.ts
@@ -0,0 +1,28 @@
+// import "dotenv/config";
+// import type { Config } from "drizzle-kit";
+
+// export default {
+//   schema: "./db/schema.ts",
+//   out: "./drizzle",
+//   driver: "pg",
+//   dbCredentials: {
+//     connectionString: process.env.DATABASE_URL!,
+//   },
+// } satisfies Config;
+
+import { config } from "dotenv";
+import { defineConfig } from "drizzle-kit";
+
+config({ path: ".env.local" });
+
+export default defineConfig({
+  dialect: "postgresql",
+  out: "./drizzle",
+  schema: "./db/schema.ts",
+  driver: "pg",
+  dbCredentials: {
+    connectionString: process.env.DATABASE_URL!,
+  },
+  verbose: true,
+  strict: true,
+});
