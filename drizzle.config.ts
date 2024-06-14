3a2f5858d124243b02628fff6c8dda622c834657
samuelsenerwa <samuelsenerwa@gmail.com>
2024-06-14 18:19:02 +0300

    chore:did some code cleanup

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -13,36 +13,3 @@ export default {
   },
   // @ts-ignore
 } satisfies Config;
-
-// import { config } from "dotenv";
-// import { defineConfig } from "drizzle-kit";
-
-// config({ path: ".env.local" });
-
-// export default defineConfig({
-//   // dialect: "postgresql",
-//   out: "./drizzle",
-//   schema: "./db/schema.ts",
-//   driver: "pg",
-//   dbCredentials: {
-//     connectionString: process.env.DATABASE_URL!,
-//   },
-//   verbose: true,
-//   strict: true,
-// });
-
-// import { defineConfig } from "drizzle-kit";
-// import dotenv from "dotenv";
-
-// dotenv.config({ path: ".env.local" });
-
-// // @ts-ignore
-// export default defineConfig({
-//   dialect: "postgresql",
-//   schema: "./db/schema.ts",
-//   out: "./drizzle",
-//   driver: "pg",
-//   dbCredentials: {
-//     url: process.env.DATABASE_URL as string,
-//   },
-// });
