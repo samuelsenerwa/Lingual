b324e769af2e1fa09246834c7711fb0468ecb0d9
samuelsenerwa <samuelsenerwa@gmail.com>
2024-06-15 22:38:28 +0300

    feat: enabled creation of user progress

--- db/queries.ts
+++ db/queries.ts
@@ -2,7 +2,7 @@ import { cache } from "react";
 import db from "@/db/drizzle";
 import { eq } from "drizzle-orm";
 import { auth } from "@clerk/nextjs/server";
-import { userProgress } from "@/db/schema";
+import { courses, userProgress } from "@/db/schema";
 
 export const getUserProgress = cache(async () => {
   const { userId } = await auth();
@@ -22,6 +22,16 @@ export const getUserProgress = cache(async () => {
 
 export const getCourses = cache(async () => {
   const data = await db.query.courses.findMany();
+
+  return data;
+});
+
+// create user progress whenever a course is clicked
+export const getCourseById = cache(async (courseId: number) => {
+  const data = await db.query.courses.findFirst({
+    where: eq(courses.id, courseId),
+    // populates units and courses
+  });
 
   return data;
 });
