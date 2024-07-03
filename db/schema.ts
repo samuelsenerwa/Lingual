d12abd258ddf3b2e66c5d6990ec60aad3bfb2a32
samuelsenerwa <samuelsenerwa@gmail.com>
2024-07-03 16:23:22 +0300

    feat: added units in the schema

--- db/schema.ts
+++ db/schema.ts
@@ -1,10 +1,20 @@
 import { relations } from "drizzle-orm";
 import { pgTable, serial, text, integer } from "drizzle-orm/pg-core";
+import { title } from "process";
 
 export const courses = pgTable("courses", {
   id: serial("id").primaryKey(),
   title: text("title").notNull(),
   imageSrc: text("image_src").notNull(),
+});
+
+export const units = pgTable("uits", {
+  id: serial("id"),
+  title: text("title").notNull(), //Unit 1
+  description: text("description").notNull(), // Learn the basics of spanish
+  courseId: integer("courseId")
+    .references(() => courses.id, { onDelete: "cascade" })
+    .notNull(),
 });
 
 export const courseRelations = relations(courses, ({ many }) => ({
