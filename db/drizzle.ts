286a58e698c5253d9e3f93600d863a3cea8aa8f0
samuelsenerwa <samuelsenerwa@gmail.com>
2024-07-08 16:40:26 +0300

    chore: refactored drizzle configurations

--- db/drizzle.ts
+++ db/drizzle.ts
@@ -1,10 +1,20 @@
+// import { neon } from "@neondatabase/serverless";
+// import { drizzle } from "drizzle-orm/neon-http";
+
+// import * as schema from "../schema";
+
+// // export const sql = neon(process.env.DATABASE_URL!);
+// const sql = neon(process.env.DATABASE_URL!);
+// const db = drizzle(sql, { schema });
+
+// export default db;
+
 import { neon } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-http";
 
-import * as schema from "../schema";
+import * as schema from "@/db/schema";
 
-// export const sql = neon(process.env.DATABASE_URL!);
-const sql = neon(process.env.DATABASE_URL!);
-const db = drizzle(sql, { schema });
+const connector = neon(process.env.DATABASE_URL!);
+const db = drizzle(connector, { schema });
 
 export default db;
--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,15 +1,26 @@
-import dotenv from "dotenv";
-import type { Config } from "drizzle-kit";
+// // import dotenv from "dotenv";
+// import "dotenv/config";
+// import type { Config } from "drizzle-kit";
 
-dotenv.config({ path: ".env.local" });
+// // dotenv.config({ path: ".env.local" });
 
-export default {
+// export default {
+//   dialect: "postgresql",
+//   schema: "./db/schema.ts",
+//   out: "./drizzle",
+//   // driver: "pg",
+//   dbCredentials: {
+//     url: process.env.DATABASE_URL!,
+//   },
+//   // @ts-ignore
+// } satisfies Config;
+
+import { defineConfig } from "drizzle-kit";
+
+export default defineConfig({
   dialect: "postgresql",
   schema: "./db/schema.ts",
-  out: "./drizzle",
-  // driver: "pg",
   dbCredentials: {
     url: process.env.DATABASE_URL!,
   },
-  // @ts-ignore
-} satisfies Config;
+});
