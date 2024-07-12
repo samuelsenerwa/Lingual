c8e2998dc5abad43bdaf024c66c2dac3538d5196
samuelsenerwa <samuelsenerwa@gmail.com>
2024-07-12 20:51:21 +0300

    feat: added footer for the lesson page

--- app/lesson/quiz.tsx
+++ app/lesson/quiz.tsx
@@ -5,6 +5,7 @@ import { useState } from "react";
 import { Header } from "./header";
 import QuestionBubble from "./question-bubble";
 import { Challenge } from "./challenge";
+import { Footer } from "./footer";
 
 type Props = {
   initialPercentage: number;
@@ -85,6 +86,10 @@ export const Quiz = ({
           </div>
         </div>
       </div>
+
+      {/* Footer Component */}
+
+      <Footer disabled={!selectedOption} status={status} onCheck={() => {}} />
     </>
   );
 };
