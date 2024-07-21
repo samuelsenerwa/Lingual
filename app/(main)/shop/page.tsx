6eb47c058a8587105720ce9da1523be654c44d43
samuelsenerwa <samuelsenerwa@gmail.com>
2024-07-21 22:09:31 +0300

    feat: added the header for the shop page

---
+++ app/(main)/shop/page.tsx
@@ -0,0 +1,28 @@
+import { StickyWrapper } from "@/components/sticky-wrapper";
+import { UserProgress } from "@/components/user-progress";
+import { getUserProgress } from "@/db/queries";
+import { redirect } from "next/navigation";
+
+const ShopPage = async () => {
+  const userProgressData = getUserProgress();
+
+  const [userProgress] = await Promise.all([userProgressData]);
+
+  //   protecting the page
+  if (!userProgress || !userProgress.activeCourse) {
+    redirect("/courses");
+  }
+
+  return (
+    <div className="flex flex-row-reverse gap-[48px] px-6">
+      <StickyWrapper>
+        <UserProgress
+          activeCourse={userProgress.activeCourse}
+          hearts={userProgress.hearts}
+          points={userProgress.points}
+          hasActiveSubscription={false}
+        />
+      </StickyWrapper>
+    </div>
+  );
+};
