13a20bf72e741655e0a34b6439ec09381910477f
samuelsenerwa <samuelsenerwa@gmail.com>
2024-08-23 22:57:14 +0300

    feat: added loading state for quest, leaderboard and shop

---
+++ app/(main)/quest/loading.tsx
@@ -0,0 +1,11 @@
+import { Loader } from "lucide-react";
+
+const Loading = () => {
+  return (
+    <div className="h-full w-full items-center justify-center">
+      <Loader className="h-6 w-6 text-muted-foreground animate-spin" />
+    </div>
+  );
+};
+
+export default Loading;
